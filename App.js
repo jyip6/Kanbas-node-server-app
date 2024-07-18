3cbc17ca4236f472d612bcadfa1d8764a48f5466
jyip6 <ye1846955797@gmail.com>
2024-07-17 18:33:45 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import express from "express";
+import Lab5 from "./Lab5/index.js";
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+ModuleRoutes(app);    
+CourseRoutes(app);  
+AssignmentRoutes(app);            
+Lab5(app);                         
+app.listen(4000);
